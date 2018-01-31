9eb21b5752a0b6c16a3694396d0d1d753d3088f3
walkthecat <58568819@qq.com>
2018-01-31 11:43:21 +0800

    高并发秒杀学习

--- src/main/resources/static/js/seckill.js
+++ src/main/resources/static/js/seckill.js
@@ -1,8 +1,81 @@
 var seckill = {
     //封装秒杀相关ajax的url
-    URL: {},
+    URL: {
+        now: function () {
+            return '/seckill/time/now';
+        },
+        exposer: function (seckillId) {
+            return '/seckill/' + seckillId + '/exposer';
+        },
+        execution: function (seckillId, md5) {
+            return '/seckill/' + seckillId + '/' + md5 + '/execution';
+        }
+    },
     validatePhone: function (phone) {
         return phone && phone.length === 11 && !isNaN(phone)
+    },
+    handleSeckill: function (seckillId, node) {
+        //获取秒杀地址，控制现实逻辑，执行秒杀
+        node.hide().html('<button class="btn btn-primary btn-lg"' +
+            ' id="killBtn">秒杀开始</button>');
+        $.post(seckill.URL.exposer(seckillId), {}, function (result) {
+            //在回调函数中，执行交互流程
+            if (result && result['success']) {
+                var exposer = result['data'];
+                if (exposer['exposed']) {
+                    //开启秒杀
+                    //获取秒杀地址
+                    var md5 = exposer['md5'];
+                    var killUrl = seckill.URL.execution(seckillId, md5);
+                    //绑定一次点击事件
+                    $('#killBtn').one('click', function () {
+                        //执行秒杀请求
+                        //1：先禁用按钮
+                        $(this).addClass('disabled');
+                        //2: 发送秒杀请求
+                        $.post(killUrl, {}, function (result) {
+                            if (result && result['success']) {
+                                var killResult = result['data'];
+                                var state = killResult['state'];
+                                var stateInfo = killResult['stateInfo'];
+                                //3：显示秒杀结果
+                                node.html('<span class="badge badge-success">' + stateInfo + '</span>');
+                            }
+                        });
+                    });
+                    node.show();
+                } else {
+                    //未开启秒杀
+                    var now = exposer['now'];
+                    var start = exposer['start'];
+                    var end = exposer['end'];
+                    //重新计算计时逻辑
+                    seckill.countdown(seckillId, now, start, end);
+                }
+            } else {
+                console.log('result:' + result);
+            }
+        });
+    },
+    countdown: function (seckillId, nowTime, startTime, endTime) {
+        var seckillBox = $('#seckill-box');
+        //时间判断
+        if (nowTime > endTime) {
+            //秒杀结束
+            seckillBox.html('秒杀结束');
+        } else if (nowTime < startTime) {
+            //秒杀未开始，计时事件绑定
+            var killTime = new Date(startTime + 1000);
+            seckillBox.countdown(killTime, function (event) {
+                var format = event.strftime('秒杀倒计时： %D天 %H时 %M分 %S秒');
+                seckillBox.html(format);
+            }).on('finish.countdown', function () {
+                seckill.handleSeckill(seckillId, seckillBox);
+            });
+        } else {
+            //秒杀开始
+            seckill.handleSeckill(seckillId, seckillBox);
+        }
     },
     //详情页秒杀逻辑
     detail: {
@@ -12,9 +85,6 @@ var seckill = {
             //规划我们的交互流程
             //在cookie中查找手机号
             var killPhone = $.cookie('killPhone');
-            var startTime = params['startTime'];
-            var endTime = params['endTime'];
-            var seckillId = params['seckillId'];
             //验证手机号
             if (!seckill.validatePhone(killPhone)) {
                 //绑定phone
@@ -37,8 +107,21 @@ var seckill = {
                         $('#killPhoneMessage').hide().html('<label class="badge badge-danger">手机号错误！</label>').show(300);
                     }
                 });
-                //已经登陆
             }
+            //已经登陆
+            //计时交互
+            var startTime = params['startTime'];
+            var endTime = params['endTime'];
+            var seckillId = params['seckillId'];
+            $.get(seckill.URL.now(), {}, function (result) {
+                if (result && result['success']) {
+                    var nowTime = result['data'];
+                    //时间判断
+                    seckill.countdown(seckillId, nowTime, startTime, endTime);
+                } else {
+                    console.log('result:' + result);
+                }
+            });
         }
     }
 };
